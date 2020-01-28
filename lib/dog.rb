170ca6a801ec605f45a47b7f1fbb7be22f36c204
Agnes Park <agnespark824@gmail.com>
2020-01-28 18:16:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   def name=(a_dogs_name)
-    @this_dogs_name = a_dogs_name
+    @name = a_dogs_name
   end
   
   def name
-    @this_dogs_name
+    @name
   end
   
   def breed=(a_dogs_breed)
